26eb1fcc1484d5d5a41cdcabbe039ef1f5d43eac
relmoo1220 <gamingwithmura@gmail.com>
2024-09-22 22:56:32 +0800

    add loading state for send button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,10 @@
   import BotReply from "../components/BotReply.svelte";
   import SectionWrapper from "../components/SectionWrapper.svelte";
   import UserRequest from "../components/UserRequest.svelte";
+
+  let isLoading = false;
+
+  async function sendMessage() {}
 </script>
 
 <div class="bg-gray-700 flex flex-col justify-center h-screen space-y-4">
@@ -18,8 +22,32 @@
     <div class="flex justify-center">
       <button
         class="p-3 border border-gray-600 rounded-md bg-gray-800 text-gray-100 w-24 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        on:click={sendMessage}
+        disabled={isLoading}
       >
-        Send
+        {#if isLoading}
+          <svg
+            class="animate-spin h-5 w-5 text-white mx-auto"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        {/if}
+        {!isLoading ? "Send" : ""}
       </button>
     </div>
   </SectionWrapper>
